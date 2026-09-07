6fa7038a1b7badcfe75c2c87e2266a70a0414156
itzel-teaches <irendon@mesacharter.org>
2026-09-07 17:24:03 -0400

    Added practice

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+TODO: 
+- Create a variable that will hold a list of student names 
+- Create a variable that will hold future users for an app we are building 
+- Create a variable that will keep count of every time Ms.Rendon says "uhm"
+- Create a variable that will hold the name of the School we attend 
+- Create a variable that will hold whether or not Ms.Rendon has assigned homework
+*/
